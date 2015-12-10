355d98189357839c6077dec3e684038ea0c9ed12
KyleJMcCullen <kylejmccullen@gmail.com>
2015-12-10 17:31:12 -0500

    oops i did it again
    uh huh

--- MasterJen.py
+++ MasterJen.py
@@ -5,7 +5,7 @@ import random
 import time
 from math import pi
 
-jen = create.Create(7, FULL_MODE)
+jen = create.Create(8, 3)
 
 def movecircle(radius_m, speed_cmps):
 	circum_cm = (2 * pi * radius_m) * 100
@@ -13,12 +13,15 @@ def movecircle(radius_m, speed_cmps):
 	deg_per_sec = -360 / circum_movetime_sec # Negative = turn clockwise
 	radius_movetime_sec = radius_m / speed_cmps
 
-	jen.move((radius_m * 1000), speed_cmps)
+	jen.move((radius_m * 100), speed_cmps)
 	time.sleep(radius_movetime_sec)
+	jen.stop()
 	jen.turn(-90)
 	jen.go(speed_cmps, deg_per_sec)
 	time.sleep(circum_movetime_sec)
+	jen.stop()
 	jen.turn(-90)
-	jen.move((radius_m * 1000), speed_cmps)
+	jen.move((radius_m * 100), speed_cmps)
 	time.sleep(radius_movetime_sec)
+	jen.stop()
 	return
